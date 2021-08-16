02ec94466591eed90a6a6baeffa8271dd17e7af0
Neerajadc <72334281+Neerajadc@users.noreply.github.com>
2021-08-16 15:58:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    cavas = createCanvas(300,3000);
+    video =  createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("results_object_name").innerHTML = results[0].label;
+    }
+}
